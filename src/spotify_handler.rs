f13af4aa1097b63106390c130f5527a443294f18
jake shoemaker <jakeshoe3@gmail.com>
2023-03-07 00:41:53 -0600

    almost done. jus tnee to figure out how to correctly authenticate for creating playlists / adding to them

--- src/spotify_handler.rs
+++ src/spotify_handler.rs
@@ -1,6 +1,6 @@
 pub mod spotify_helper {
     use rspotify::{
-        model::{SearchType, Country, Market, SearchResult, PrivateUser, TrackId},
+        model::{SearchType, Country, Market, SearchResult, PrivateUser, TrackId, PlaylistId, PlaylistResult},
         prelude::*,
         scopes, AuthCodeSpotify, Credentials, OAuth,
     };
@@ -9,7 +9,8 @@ pub mod spotify_helper {
         api: AuthCodeSpotify,
         user: PrivateUser,
         market: Market,
-        playlist_id: String
+        playlist_id: Option<PlaylistId<'static>>,
+        track_ids: Vec<PlayableId<'static>>
     }
 
     impl SpotifyHandler {
@@ -17,23 +18,20 @@ pub mod spotify_helper {
             let api = SpotifyHandler::setup_spotify_authcode().await;
             let user = SpotifyHandler::get_authenticated_user(api.clone()).await;
             let market = Market::Country(Country::UnitedStates);
-            let playlist_id = "".to_string();
+            let playlist_id = None;
+            let track_ids = vec![];
             let s = Self {
                 api,
                 user,
                 market,
-                playlist_id
+                playlist_id,
+                track_ids
             };
 
             return s;
         }
 
-        pub fn test_user_collected(&self) {
-            dbg!(&self.user);
-        } 
-
-        // guessing we need a spotify id here
-        pub async fn get_track(&self, query: &String) /* todo return something */ {
+        pub async fn search_for_track(&mut self, query: &String) {
             let potential_track = match 
                 self.api.search(
                     query, 
@@ -47,20 +45,52 @@ pub mod spotify_helper {
                 SearchResult::Tracks(t) => t,
                 _ => todo!("finish mapping this")
             };
-
-            let t1 = potential_track.items.first().unwrap();
-            dbg!(t1);
-
-            // these are handy to test with 
-            // let test_track_name = &"BoTalks - F*ck It (feat. Caroline Pennell)";
-            // let test_track_name2 = &"deadmau5 - My Heart Has Teeth (feat. Skylar Grey)";
+            let id = potential_track.items
+                .first()
+                .unwrap()
+                .id
+                .to_owned()
+                .unwrap();
+            
+            self.track_ids.push(PlayableId::Track(id.clone_static()));
         }
 
+        //spotify.user_playlist_create(h, name, j, collaborative, description)
+        pub async fn create_user_playlist(&mut self) {
+            // todo: nice user interaction asking for playlist name /description using clap
+            let name: String = "bangers".to_string();
+            let description = "created from my commandline using rust and a youtube link".to_string();
+            let user_id = &self.user.id;
+            let playlist = self.api.user_playlist_create(
+                user_id.to_owned(), &name, Some(false), Some(false), Some(&description))
+                    .await
+                    .unwrap();
+
+            self.set_playlist_id(&playlist.id);
+        }
+
+        //spotify.playlist_add_items(playlist_id, items, position)
+        pub async fn add_tracks_to_playlist(mut self) -> PlaylistResult {
+            // if playlist is none, go ahead and create the playlis
+            if self.playlist_id == None {
+                self.create_user_playlist().await;
+            }
+
+            let playlist_id = self.playlist_id.to_owned().unwrap();
+            let tracks = self.track_ids.into_iter();
+            return self.api.playlist_add_items(playlist_id, tracks, None)
+                .await
+                .unwrap();
+        }
 
         // private methods
+        fn set_playlist_id(&mut self, id: &PlaylistId<'static>) {
+            self.playlist_id = Some(id.clone());
+        }
+
         async fn setup_spotify_authcode() -> AuthCodeSpotify {
             let creds: Credentials = Credentials::from_env().unwrap();
-            let oauth: OAuth = OAuth::from_env(scopes!("user-read-currently-playing")).unwrap();
+            let oauth: OAuth = OAuth::from_env(scopes!("playlist-modify-private")).unwrap();
             let spotify: AuthCodeSpotify = AuthCodeSpotify::new(creds, oauth);
             //  obtain access token, and allow spotify to get a token itself
             let url = match spotify.get_authorize_url(false) {
@@ -83,5 +113,6 @@ pub mod spotify_helper {
             return user;
         }
     }
+
 }
 
